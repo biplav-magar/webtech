7ea651162553f65cabb159e1975aec28cc0137a1
biplav-magar <biplavwork@gmail.com>
2025-12-08 09:14:40 +0545

    Add files via upload

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+$name="Bernhardt";
+echo "Welcome to ". $name. " college ";
+$number1=10;
+$number2=15;
+
+echo"addition of two numbers=";
+echo"&nbsp";//space in html
+echo $number1+$number2;
+echo"&nbsp";
+if($number1>$number2){
+    echo"number 1 is greater than number 2";
+}else{
+    echo "number 2 is greater";
+}
+?>
